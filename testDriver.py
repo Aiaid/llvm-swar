71089ac2559ff737255613351c96a4d43b2db3d0
Cole Hao Wu <hwa129@sfu.ca>
2021-08-14 21:34:52 -0700

    test driver update

--- testDriver.py
+++ testDriver.py
@@ -1,12 +1,26 @@
-import os
-import subprocess
+import random
+import subprocess, sys, getopt
 
-fileName = input("Abs Path for testing file:")
-iterations = int(input("Test Times:"))
+fileName = ""
+iterations = 0
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "f:i:")
+except getopt.GetoptError:
+    print ('testDriver.py -f <abs path of inputfile> -i <number of iterations>')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-f':
+        fileName = arg
+    elif opt == '-i':
+        iterations = int(arg)
+
 accTime = 0.0
 
 for i in range(iterations):
-    cmd = 'time lli-12 -force-interpreter=true ' + fileName
+    rand1 = random.getrandbits(128)
+    rand2 = random.getrandbits(128)
+    cmd = 'time lli-12 ' + fileName + ' ' + str(rand1) + ' ' + str(rand2)
     stat = subprocess.run(cmd, shell=True, executable="/bin/fish", capture_output=True)
     # print(stat)
     x = 182
